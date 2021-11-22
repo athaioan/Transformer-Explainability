f4deb2ccf8cd0309c1fcbd50919d86feb712fa45
Ioannis Athanasiadis <38070221+athaioan@users.noreply.github.com>
2021-11-22 13:57:38 +0100

    Add files via upload

---
+++ ours/networks/network.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+
+class ViT_model(nn.Module):
+    def __init__(self, n_classes):
+        super(ViT_model, self).__init__()
+
+        ## TODO architecture
+
+
+
+
+    def forward(self):
+
+    def extract_cam(self):
+
+    def load_pretrained(self):
+
+
