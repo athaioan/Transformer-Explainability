22c457a6f668cdee20fc1763bcf0e64fff320e76
Ioannis Athanasiadis <38070221+athaioan@users.noreply.github.com>
2021-11-22 13:58:05 +0100

    Add files via upload

--- ours/demo_eval/demo.py
+++ ours/demo_eval/demo.py
@@ -2,18 +2,22 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
-from types import SimpleNamespace
-from utils import *
 import torchvision.transforms as transforms
+from types import SimpleNamespace
+
+from utils import *
+from network import ViT_model as model
+
 
 ### Setting arguments
 args = SimpleNamespace(batch_size=1,
-                       input_dim=226,
-                       input_dim_crop=224,
+                       input_dim=224,
                        pretrained_weights="pretrained/vgg16_20M.pth",
                        val_set="ILSVRC2012_img_val",
-                       test_set="test_new.txt",
-                       labels_dict="val_labels_dict.npy")
+                       labels_dict="val_labels_dict.npy",
+                       device=torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+                       )
+
 
 
 import pickle
@@ -38,18 +42,27 @@ dict = {}
 
 normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
 transform = transforms.Compose([
-    transforms.Resize(args.input_dim),
-    transforms.CenterCrop(args.input_dim_crop),
+    transforms.Resize((args.input_dim, args.input_dim)),
     transforms.ToTensor(),
     normalize,
 ])
 
+# transform_gt_mask = transforms.Compose([
+#     transforms.Resize(args.input_dim),
+#     transforms.ToTensor(),
+# ])
 
 
 ## Constructing the training loader
-val_loader = ImageNetVal(args.val_set, args.labels_dict, transform) ## loading val split (50.000)
+val_loader = ImageNetVal(args.val_set, args.labels_dict, args.device, transform) ## loading val split (50.000)
 val_loader = DataLoader(val_loader, batch_size=args.batch_size, shuffle=False)
 
-for data in val_loader:
+for index, data in enumerate(val_loader):
+
+    ## TODO model
+    img = data[0]
+    label = data[1]
+
+    model.extract_cam(img,label)
 
     print("")
