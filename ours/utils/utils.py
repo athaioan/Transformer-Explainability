ebd77a175ac43a7140b54505cff8a98ab4d9570e
Ioannis Athanasiadis <38070221+athaioan@users.noreply.github.com>
2021-11-22 13:56:55 +0100

    Add files via upload

--- ours/utils/utils.py
+++ ours/utils/utils.py
@@ -9,24 +9,26 @@ import matplotlib.pyplot as plt
 class ImageNetVal(Dataset):
 
     ### Overwriting some functions of Dataset build in class
-    def __init__(self, img_folder, labels_dict, transform):
+    def __init__(self, img_folder, labels_dict, device, transform):
 
         self.labels_dict = np.load(labels_dict, allow_pickle=True)
         self.transform = transform
         self.img_names = os.listdir(img_folder)
         self.img_names = np.asarray([img_folder+"/"+current_img for current_img in self.img_names])
+        self.device = device
 
 
     def __len__(self):
         return len(self.img_names)
 
     def __getitem__(self, idx):
+
         current_path = self.img_names[idx]
+        img_id = current_path.split("/")[-1]
 
         img_orig = Image.open(current_path)
-
         img = self.transform(img_orig)
 
+        label = torch.IntTensor([self.labels_dict[img_id]])
 
-
-        return img_meta, img, label
+        return img.to(self.device), label.to(self.device)
