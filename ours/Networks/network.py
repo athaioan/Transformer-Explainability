2e8b8f29f5e44b31ef42aa0922de399407445b43
Ioannis Athanasiadis <38070221+athaioan@users.noreply.github.com>
2021-11-24 19:12:01 +0100

    Add files via upload

--- ours/Networks/network.py
+++ ours/Networks/network.py
@@ -2,11 +2,14 @@ import torch
 from torch import nn
 import matplotlib.pyplot as plt
 from overwritten_layers import *
-from ours.Utils.utils import *
+from utils import *
+from einops import rearrange
+
 
 class ViT_model(nn.Module):
     def __init__(self, n_classes=1000, img_size=(224, 224), patch_size=16, in_ch=3, embed_dim=768):
         super(ViT_model, self).__init__()
+
 
         self.n_classes = n_classes
         self.img_size = img_size
@@ -14,14 +17,18 @@ class ViT_model(nn.Module):
         self.in_ch = in_ch
         self.embed_dim = embed_dim
 
-        self.patching = Img_to_Patch(img_size, patch_size, in_ch, embed_dim)
+        self.add = Add()
+        self.patching = Img_to_patch(img_size, patch_size, in_ch, embed_dim)
 
         self.positional_embed = nn.Parameter(torch.zeros(1, self.patching.n_patches + 1, embed_dim))
         self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
 
+        set_seeds(0)
         no_grad_trunc_normal_(self.positional_embed, std=.02)
         no_grad_trunc_normal_(self.cls_token, std=.02)
 
+        self.input_grad = None
+        self.att = Attention_layer(768, n_heads=12, QKV_bias=True, att_dropout=0., proj_dropout=0.)
 
     def forward(self, x):
         batch_size = x.shape[0]
@@ -29,9 +36,17 @@ class ViT_model(nn.Module):
 
         cls_token = self.cls_token.expand(batch_size, -1, -1)# from Phil Wang
         x = torch.cat((cls_token, x), dim=1)
-        x = x + self.positional_embed
+        x = self.add([x, self.positional_embed])# x+= self.positional_embed
+
+        x.register_hook(self.store_input_grad) ## When computing the grad wrt to the input x, store that grad to the model.input_grad
+
+        self.att(x)
 
         print("")
+
+    def store_input_grad(self, grad):
+        self.input_grad = grad
+
 
     def extract_cam(self):
         print("")
@@ -40,9 +55,9 @@ class ViT_model(nn.Module):
         print("")
 
 
-class Img_to_Patch(nn.Module):
+class Img_to_patch(nn.Module):
     def __init__(self, img_size=(224, 224), patch_size=16, input_ch=3, embed_size=768):
-        super(Img_to_Patch, self).__init__()
+        super(Img_to_patch, self).__init__()
         self.img_size = img_size
         self.patch_size = patch_size
         self.input_ch = input_ch
@@ -57,3 +72,56 @@ class Img_to_Patch(nn.Module):
 
         x = self.conv2d(x).flatten(2).transpose(1, 2)
         return x
+
+class Attention_layer(nn.Module):
+
+    def __init__(self, embed_size, n_heads=12, QKV_bias=False, att_dropout=0., proj_dropout=0.):
+        super().__init__()
+        self.n_heads = n_heads
+        head_dim = embed_size // n_heads
+        # NOTE scale factor was wrong in my original version, can set manually to be compat with prev weights
+        self.scale = head_dim ** -0.5
+
+        self.qkv = Linear(embed_size, embed_size * 3, bias=QKV_bias)
+
+        # A = Q*K^T
+        self.matmul1 = Matmul(transpose=True)
+        self.matmul2 = Matmul(transpose=False)
+
+        self.softmax = Softmax()
+
+
+        # self.matmul1 = matmul()
+        # # attn = A*V
+        # self.matmul2 = einsum('bhij,bhjd->bhid')
+
+        # self.qkv = Linear(dim, dim * 3, bias=qkv_bias)
+        # self.att_drop = Dropout(att_dropout)
+        # self.proj = Linear(dim, dim)
+        # self.proj_drop = Dropout(proj_drop)
+        # self.softmax = Softmax(dim=-1)
+        #
+        # self.attn_cam = None
+        # self.attn = None
+        # self.v = None
+        # self.v_cam = None
+        # self.attn_gradients = None
+
+    def store_v(self, v):
+        self.v = v
+
+    def forward(self,x):
+        batch, n, embed_size = x.shape
+        qkv = self.qkv(x)
+
+        ## ours
+        qkv = torch.reshape(qkv, (batch, n, 3, self.n_heads, embed_size//self.n_heads))
+        q, k, v = qkv.permute(2, 0, 3, 1, 4)
+
+        self.store_v(v)
+
+        scaled_products = self.matmul1([q, k]) * self.scale
+
+        attn = self.softmax(scaled_products)
+
+        print("")
--- ours/Networks/overwritten_layers.py
+++ ours/Networks/overwritten_layers.py
@@ -6,3 +6,56 @@ class Conv2d(nn.Conv2d):
 
     def test_funct(self):
         print("")
+
+    #TODO REL_PROP
+
+class Add(nn.Module): ## Change when implementing  Rel_pro
+
+    def forward(self, inputs):
+        return torch.add(*inputs)
+
+    def test_funct(self):
+        print("")
+
+    #TODO REL_PROP
+
+
+class Clone(nn.Module): ## Change when implementing  Rel_pro
+
+    def forward(self, input, num):
+
+        self.num = num
+        clone_list = []
+        for _ in range(num):
+            clone_list.append(input)
+
+        return clone_list
+
+    #TODO REL_PROP
+
+class Linear(nn.Linear):
+
+
+    def test_funct(self):
+        print("")
+
+    #TODO REL_PROP
+
+class Matmul(nn.Module):
+
+    def __init__(self, transpose=False):
+        super().__init__()
+        self.transpose = transpose
+
+    def forward(self, X):
+        if self.transpose:
+            return torch.matmul(X[0], torch.transpose(X[1], 2, 3))
+        else:
+            return torch.matmul(X[0], X[1])
+
+
+    #TODO REL_PROP
+
+
+class Softmax(nn.Softmax):
+    pass
