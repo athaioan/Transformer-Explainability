d921ab86fd00ad508f379681a85bdb6a9e9d02fa
georgmosh <georgiosmoshovis@gmail.com>
2021-11-30 11:56:22 +0100

    Loading data

--- toy_example_matfile.py
+++ toy_example_matfile.py
@@ -1,6 +1,78 @@
 import mat73
+import torch
+import torchvision.transforms as transforms
 from matplotlib import pyplot as plot
+from types import SimpleNamespace
+from PIL import Image
 
-mat = mat73.loadmat('/home/georg_mosh/Documents/gtsegs_ijcv.mat')
-plot.imshow(mat['value']['img'][0])
-plot.show()
+args = SimpleNamespace(batch_size=1,
+                       input_dim=224,
+                       pretrained_weights="pretrained/vgg16_20M.pth",
+                       val_set="ILSVRC2012_img_val",
+                       labels_dict="val_labels_dict.npy",
+                       device=torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+                       )
+
+class Loader:
+    def __init__(self, path, device, transform_img, transform_seg_mask):
+        # Load the data
+        data = mat73.loadmat(path)
+        data = data['value']
+
+        # Store the device
+        self.device = device
+
+        # Extract arguments
+        self.n_images = data['n'].item()
+        self.images = data['img']
+        self.image_ids = data['id']
+        self.seg_masks = data['gt']
+        self.targets = data['target']
+
+        # Specify transforms
+        self.transform_img = transform_img
+        self.transform_seg_mask = transform_seg_mask
+
+    def __len__(self):
+        return self.n_images
+
+    def __getitem__(self, idx):
+
+        # The image with index idx
+        img_orig = self.images[idx]
+        n_channels = img_orig.shape[-1]     # todo --> check
+        if n_channels == 1:
+            img_orig = img_orig.convert(mode='RGB')
+
+        img_pil = Image.fromarray(img_orig)
+        img_trans = self.transform_img(img_pil)
+
+        # The ground truth segmentation mask
+        seg_mask_orig = self.seg_masks[idx]
+        seg_mask_pil = Image.fromarray(seg_mask_orig)
+        seg_mask_trans = self.transform_seg_mask(seg_mask_pil)
+
+        label = self.targets[idx]
+
+        id = self.image_ids[idx]
+
+        return img_trans.to(self.device), seg_mask_trans.to(self.device), label.to(self.device), id.to(self.device)
+
+path = r'C:\Users\georg\Documents\KTH_ML_Master\Deep Learning Advanced Course\Project\Datasets\gtsegs_ijcv.mat'
+
+normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+transform = transforms.Compose([
+    transforms.Resize((args.input_dim, args.input_dim)),
+    transforms.ToTensor(),
+    normalize,
+])
+
+transform_gt_mask = transforms.Compose([
+    transforms.Resize(args.input_dim),
+    transforms.ToTensor(),
+])
+
+piece_of_crap = Loader(path, args.device, transform, transform_gt_mask)
+one_image = piece_of_crap[0]
+
+print("DONE")
