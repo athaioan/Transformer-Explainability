273d49b89d46fc5586584c55979e5f79626d601e
Ioannis Athanasiadis <38070221+athaioan@users.noreply.github.com>
2021-12-02 11:08:48 +0100

    Add files via upload

--- baselines/ViT/pertubation_eval_from_hdf5.py
+++ baselines/ViT/pertubation_eval_from_hdf5.py
@@ -1,7 +1,6 @@
 
 import torch
 import os
-from tqdm import tqdm
 import numpy as np
 import argparse
 
@@ -11,8 +10,8 @@ from ViT_new import vit_base_patch16_224
 # from models.vgg import vgg19
 import glob
 
-from dataset.expl_hdf5 import ImagenetResults
-
+from expl_hdf5 import ImagenetResults
+import matplotlib.pyplot as plt
 
 def normalize(tensor,
               mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]):
@@ -44,7 +43,7 @@ def eval(args):
     prob_diff_pertub = np.zeros((9, len(imagenet_ds)))
     perturb_index = 0
 
-    for batch_idx, (data, vis, target) in enumerate(tqdm(sample_loader)):
+    for batch_idx, (data, vis, target) in enumerate(sample_loader):
         # Update the number of samples
         num_samples += len(data)
 
@@ -56,10 +55,10 @@ def eval(args):
         # Compute model accuracy
         pred = model(norm_data)
         pred_probabilities = torch.softmax(pred, dim=1)
-        pred_org_logit = pred.data.max(1, keepdim=True)[0].squeeze(1)
-        pred_org_prob = pred_probabilities.data.max(1, keepdim=True)[0].squeeze(1)
-        pred_class = pred.data.max(1, keepdim=True)[1].squeeze(1)
-        tgt_pred = (target == pred_class).type(target.type()).data.cpu().numpy()
+        pred_org_logit = pred.data.max(1, keepdim=True)[0].squeeze(1) ## max value
+        pred_org_prob = pred_probabilities.data.max(1, keepdim=True)[0].squeeze(1) ## max value probability
+        pred_class = pred.data.max(1, keepdim=True)[1].squeeze(1) ## argmax index of class
+        tgt_pred = (target == pred_class).type(target.type()).data.cpu().numpy() ## flag True when pred = gt
         num_correct_model[model_index:model_index+len(tgt_pred)] = tgt_pred
 
         probs = torch.softmax(pred, dim=1)
@@ -120,24 +119,29 @@ def eval(args):
         model_index += len(target)
         perturb_index += len(target)
 
-    np.save(os.path.join(args.experiment_dir, 'model_hits.npy'), num_correct_model)
-    np.save(os.path.join(args.experiment_dir, 'model_dissimilarities.npy'), dissimilarity_model)
-    np.save(os.path.join(args.experiment_dir, 'perturbations_hits.npy'), num_correct_pertub[:, :perturb_index])
-    np.save(os.path.join(args.experiment_dir, 'perturbations_dissimilarities.npy'), dissimilarity_pertub[:, :perturb_index])
-    np.save(os.path.join(args.experiment_dir, 'perturbations_logit_diff.npy'), logit_diff_pertub[:, :perturb_index])
-    np.save(os.path.join(args.experiment_dir, 'perturbations_prob_diff.npy'), prob_diff_pertub[:, :perturb_index])
+    # np.save(os.path.join(args.experiment_dir, 'model_hits.npy'), num_correct_model)
+    # np.save(os.path.join(args.experiment_dir, 'model_dissimilarities.npy'), dissimilarity_model)
+    # np.save(os.path.join(args.experiment_dir, 'perturbations_hits.npy'), num_correct_pertub[:, :perturb_index])
+    # np.save(os.path.join(args.experiment_dir, 'perturbations_dissimilarities.npy'), dissimilarity_pertub[:, :perturb_index])
+    # np.save(os.path.join(args.experiment_dir, 'perturbations_logit_diff.npy'), logit_diff_pertub[:, :perturb_index])
+    # np.save(os.path.join(args.experiment_dir, 'perturbations_prob_diff.npy'), prob_diff_pertub[:, :perturb_index])
 
     print(np.mean(num_correct_model), np.std(num_correct_model))
     print(np.mean(dissimilarity_model), np.std(dissimilarity_model))
     print(perturbation_steps)
     print(np.mean(num_correct_pertub, axis=1), np.std(num_correct_pertub, axis=1))
     print(np.mean(dissimilarity_pertub, axis=1), np.std(dissimilarity_pertub, axis=1))
-
+    Yall = np.vstack((num_correct_model, num_correct_pertub))
+    Y_pertub = num_correct_pertub
+    AUC_all = np.trapz(np.mean(Yall, axis=1), dx=0.1)
+    AUC_pertub = np.trapz(np.mean(Y_pertub, axis=1), dx=0.1)
+    print(AUC_all)
+    print(AUC_pertub)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train a segmentation')
     parser.add_argument('--batch-size', type=int,
-                        default=16,
+                        default=1,
                         help='')
     parser.add_argument('--neg', type=bool,
                         default=True,
@@ -150,7 +154,7 @@ if __name__ == "__main__":
                         choices=['per', '100'],
                         help='')
     parser.add_argument('--method', type=str,
-                        default='grad_rollout',
+                        default='transformer_attribution',
                         choices=['rollout', 'lrp', 'transformer_attribution', 'full_lrp', 'v_gradcam', 'lrp_last_layer',
                                  'lrp_second_layer', 'gradcam',
                                  'attn_last_layer', 'attn_gradcam', 'input_grads'],
@@ -227,7 +231,9 @@ if __name__ == "__main__":
     sample_loader = torch.utils.data.DataLoader(
         imagenet_ds,
         batch_size=args.batch_size,
-        num_workers=2,
+        num_workers=0,
         shuffle=False)
 
     eval(args)
+
+    print("")
