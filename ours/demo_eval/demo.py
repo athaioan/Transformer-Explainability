c595722fc18e26652139d40bc938f137b591ebd6
Ioannis Athanasiadis <38070221+athaioan@users.noreply.github.com>
2021-12-02 18:41:06 +0100

    Add files via upload

--- ours/demo_eval/demo.py
+++ ours/demo_eval/demo.py
@@ -4,48 +4,35 @@ import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from types import SimpleNamespace
+from utils import *
+from network import ViT_model
+import pickle
 
-# from utils import * # Ioannis
-from ours.Utils.utils import * # Georgios
-# from network import ViT_model # Ioannis
-from ours.Networks.network import ViT_model # Georgios
+# from ours.Utils.utils import * # Georgios
+# from ours.Networks.network import ViT_model # Georgios
 
 ### Setting arguments
 args = SimpleNamespace(batch_size=1,
                        input_dim=224,
-                       pretrained_weights="pretrained/vgg16_20M.pth",
-                       val_set="ILSVRC2012_img_val",
+                       pretrained_weights="saved_weights.pth",
+                       # val_set="ILSVRC2012_img_val",
+                       val_set="C:/Users/johny/Desktop/Transformer-Explainability-main/Transformer-Explainability-main\Dataset_1/val/n01440764/",
                        val_set_semg="gtsegs_ijcv.mat",
                        labels_dict="val_labels_dict.npy",
                        device=torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
                        )
 
-##### Georgios computer
-args.val_set_semg = r'C:\Users\georg\Documents\KTH_ML_Master\Deep Learning Advanced Course\Project\Datasets\gtsegs_ijcv.mat'
-#######################
-
-
-import pickle
+# args.val_set_semg = r'C:\Users\georg\Documents\KTH_ML_Master\Deep Learning Advanced Course\Project\Datasets\gtsegs_ijcv.mat'
 
 dict = {}
 
-# with open('C:/Users/johny/Desktop/Transformer-Explainability-main/Transformer-Explainability-main/val_labels.txt') as file:
-#     lines = file.readlines()
-#     lines = [line.rstrip() for line in lines]
-#     for current_line in lines:
-#
-#         key, value = current_line.split(" ")
-#         value = int(value)
-#         dict[key] = value
-#
-# with open("val_labels_dict.npy", 'wb') as f:
-#     pickle.dump(dict, f)
-
-# loading
-# with open(args.labels_dict, 'rb') as f:
-#     ret_di = pickle.load(f)
-
 normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+
+transform_perturb = transforms.Compose([
+    transforms.Resize((args.input_dim, args.input_dim)),
+    transforms.ToTensor(),
+])
+
 transform = transforms.Compose([
     transforms.Resize((args.input_dim, args.input_dim)),
     transforms.ToTensor(),
@@ -59,17 +46,17 @@ transform_gt_mask = transforms.Compose([
 
 
 # # # ## Constructing the validation loader
-# val_loader = ImageNetVal(args.val_set, args.labels_dict, args.device, transform) ## loading val split (50.000)
-# val_loader = DataLoader(val_loader, batch_size=args.batch_size, shuffle=False)
+val_loader = ImageNetVal(args.val_set, args.labels_dict, args.device, transform_perturb) ## loading val split (50.000)
+val_loader = DataLoader(val_loader, batch_size=args.batch_size, shuffle=False)
 
 # Constructing the validation segm loader
-val_loader = ImageNetSegm(args.val_set_semg, args.device, transform, transform_gt_mask) ## loading val split (50.000)
-val_loader = DataLoader(val_loader, batch_size=args.batch_size, shuffle=False)
+# val_loader = ImageNetSegm(args.val_set_semg, args.device, transform, transform_gt_mask) ## loading val split (50.000)
+# val_loader = DataLoader(val_loader, batch_size=args.batch_size, shuffle=False)
 
 
 ## Initialize model
 model = ViT_model(device=args.device) ## TODO inster the number of class imagenet:1000 , PascalVOC: 18
-model.load_pretrained("saved_weights.pth")
+model.load_pretrained(args.pretrained_weights)
 model.eval()
 model.zero_grad()
 
@@ -97,6 +84,10 @@ model.zero_grad()
 #     print("")
 
 
-pixAcc, mIoU, mAp = model.extract_metrics(val_loader)
+# pixAcc, mIoU, mAp = model.extract_metrics(val_loader)
+# print(pixAcc, mIoU, mAp)
+# 0.7971615078627627 0.6198494989665153 0.8604044712192599
 
-print(pixAcc, mIoU, mAp)
+
+AUC = model.extract_AUC(val_loader, normalize, positive=True, vis_class_top=True)
+print(AUC)
---
+++ ours/demo_eval/demo_PascalVoc.py
@@ -0,0 +1,98 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from types import SimpleNamespace
+from utils import *
+from network import ViT_model
+import pickle
+
+# from ours.Utils.utils import * # Georgios
+# from ours.Networks.network import ViT_model # Georgios
+
+### Setting arguments
+args = SimpleNamespace(batch_size=4,
+                       input_dim=448,
+                       pretrained_weights="saved_weights.pth",
+                       epochs=20,
+                       lr=0.04,
+                       weight_decay=5e-4,
+                       voc12_img_folder="C:/Users/johny/Desktop/KTH COURSES/Deep Learning/Group90_Project/VOCdevkit/VOC2012/JPEGImages/",
+                       train_set="C:/Users/johny/Desktop/KTH COURSES/Deep Learning/Group90_Project/VOCdevkit/VOC2012/ImageSets/Segmentation/train_augm.txt",
+                       val_set="C:/Users/johny/Desktop/KTH COURSES/Deep Learning/Group90_Project/VOCdevkit/VOC2012/ImageSets/Segmentation/val.txt",
+                       labels_dict="C:/Users/johny/Desktop/KTH COURSES/Deep Learning/Group90_Project/VOCdevkit/VOC2012/cls_labels.npy",
+                       device=torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu"),
+                       step_update_lr=4,
+                       )
+
+lines= [ ]
+with open(r"C:\Users\johny\Desktop\KTH COURSES\Deep Learning\Group90_Project\VOCdevkit\VOC2012\ImageSets\Segmentation\train_aug.txt") as file:
+    for line in file:
+        line = line.strip() #or some other preprocessing
+        lines.append(line) #storing everything in memory!
+
+normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+train_loader = PascalVOC2012(args.train_set, args.labels_dict, args.voc12_img_folder, args.input_dim, args.device,
+                              transform=transforms.Compose([
+                              transforms.ToTensor(),
+                              transforms.RandomHorizontalFlip(p=0.5),
+                              transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.1),
+                              normalize,
+                              ]))
+train_loader = DataLoader(train_loader, batch_size=args.batch_size, shuffle=True)
+
+val_loader = PascalVOC2012(args.val_set, args.labels_dict, args.voc12_img_folder, args.input_dim, args.device,
+                              transform=transforms.Compose([
+                              transforms.ToTensor(),
+                              normalize,
+                              ]))
+val_loader = DataLoader(val_loader, batch_size=args.batch_size, shuffle=False)
+
+
+
+
+## Initialize model
+model = ViT_model(img_size=(448, 448), patch_size=32, n_classes=20, max_epochs=args.epochs, device=args.device) ## TODO inster the number of class imagenet:1000 , PascalVOC: 18
+model.load_pretrained("saved_weights.pth")
+model.session_name = "PascalVOC_classification"
+model.eval()
+
+if not os.path.exists(model.session_name):
+    os.makedirs(model.session_name)
+
+
+# Prepare optimizer and scheduler
+optimizer = torch.optim.SGD(model.parameters(),
+                            lr=args.lr,
+                            momentum=0.9,
+                            weight_decay=args.weight_decay)
+
+
+
+for index in range(model.max_epochs):
+
+    if model.current_epoch % args.step_update_lr == args.step_update_lr - 1:
+        for g in optimizer.param_groups:
+            g['lr'] = g['lr'] / 3
+
+
+    print("Training epoch...")
+    model.train_epoch(train_loader, optimizer)
+
+    print("Validating epoch...")
+    model.val_epoch(val_loader)
+
+    model.visualize_graph()
+
+    if model.val_history["loss"][-1] < model.min_val:
+        print("Saving model...")
+        model.min_val = model.val_history["loss"][-1]
+
+        torch.save(model.state_dict(), model.session_name+"/stage_1.pth")
+
+#
+# ## Constructing the validation loader
+# val_loader = VOC2012Dataset(args.val_set, args.labels_dict, args.voc12_img_folder, args.input_dim)
+# val_loader = DataLoader(val_loader, batch_size=args.batch_size, shuffle=False) ## no point in shufflying the validation data
+#
