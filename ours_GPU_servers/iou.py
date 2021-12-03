9c73fd71fb188d1eab384e270e5f7d10639c1397
Georgios Moschovis <geomos@kth.se>
2021-12-03 13:12:12 +0100

    Update iou.py

--- ours_GPU_servers/iou.py
+++ ours_GPU_servers/iou.py
@@ -1,9 +1,9 @@
 import torch
 import numpy as np
-# import metric
-# from confusionmatrix import ConfusionMatrix
-from ours.Utils.confusionmatrix import * # Georgios
-from ours.Utils.metric import * # Georgios
+import metric
+from confusionmatrix import ConfusionMatrix
+# from ours.Utils.confusionmatrix import * # Georgios
+# from ours.Utils.metric import * # Georgios
 
 class IoU(Metric):
     """Computes the intersection over union (IoU) per class and corresponding
