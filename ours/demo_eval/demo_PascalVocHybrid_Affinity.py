8a66582930ba359b0312ed352dd870739ede197b
Ioannis Athanasiadis <38070221+athaioan@users.noreply.github.com>
2021-12-13 01:43:44 +0100

    Add files via upload

--- ours/demo_eval/demo_PascalVocHybrid_Affinity.py
+++ ours/demo_eval/demo_PascalVocHybrid_Affinity.py
@@ -4,40 +4,30 @@ import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from types import SimpleNamespace
-from tqdm import trange
-# from utils import *
-# from network import ViT_hybrid_model_Affinity
+from utils import *
+from network import ViT_hybrid_model_Affinity
 import pickle
 
-from ours.Utils.utils import * # Georgios
-from ours.Networks.network import * # Georgios
-
-import warnings
-warnings.filterwarnings("ignore")
+# from ours.Utils.utils import * # Georgios
+# from ours.Networks.network import ViT_model # Georgios
 
 
 ### Setting arguments
-args = SimpleNamespace(batch_size=1,
+args = SimpleNamespace(batch_size=4,
                        input_dim=448,
                        # pretrained_weights="saved_weights_VOC.pth",
-                       pretrained_weights="C:/Users/georg/PycharmProjects/Transformer-Explainability/ours/HybridTraining/stage_1.pth",
-                       epochs=50,
+                       pretrained_weights="PascalVOC_classification_Hybrid_1/stage_1.pth",
+                       epochs=15,
                        lr=5e-3,
-                       momentum=0.9,
                        weight_decay=1e-4,
-                       VocClassList=r"C:/Users/georg/PycharmProjects/Transformer-Explainability/ours/txts/pvclasses.txt",
-                       voc12_img_folder="C:/Users/georg/Documents/KTH_ML_Master/Deep Learning Advanced Course/Project/Datasets/VOCdevkit/VOC2012/JPEGImages/",
-                       train_set="C:/Users/georg/PycharmProjects/Transformer-Explainability/ours/txts/val.txt", ## TODO train set
-                       # val_set=r"C:\Users\johny\Desktop\Transformer-Explainability-main\ours\VOCdevkit\VOC2012\ImageSets\Segmentation\val.txt",
-                       low_cams_fold = "C:/Users/georg/PycharmProjects/Transformer-Explainability/ours/HybridTraining/crf_lows/",
-                       high_cams_fold = "C:/Users/georg/PycharmProjects/Transformer-Explainability/ours/HybridTraining/crf_highs/",
-                       home_fold = "C:/Users/georg/PycharmProjects/Transformer-Explainability/ours/demo_eval/",
+                       VocClassList="C:/Users/johny/Desktop/Transformer-Explainability-main/ours/PascalVocClasses.txt",
+                       voc12_img_folder="VOCdevkit/VOC2012/JPEGImages/",
+                       train_set=r"C:\Users\johny\Desktop\Transformer-Explainability-main\ours\VOCdevkit\VOC2012\ImageSets\Segmentation\val.txt", ## TODO train set
+                       val_set=r"C:\Users\johny\Desktop\Transformer-Explainability-main\ours\VOCdevkit\VOC2012\ImageSets\Segmentation\val.txt",
+                       low_cams_fold = "C:/Users/johny/Desktop/Transformer-Explainability-main/ours/val_cams/crf_lows/",
+                       high_cams_fold = "C:/Users/johny/Desktop/Transformer-Explainability-main/ours/val_cams/crf_highs/",
                        device=torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu"),
-                       radius=5,
-                       crop_size=448,
-                       tol=1e-5
                        )
-
 
 
 normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
@@ -57,7 +47,23 @@ train_loader = PascalVOC2012Affinity(args.train_set,  args.voc12_img_folder, arg
                              ]),
                              both_transform=transforms.RandomHorizontalFlip(p=0.5))
 
-train_loader = DataLoader(train_loader, batch_size=args.batch_size, shuffle=False)
+train_loader = DataLoader(train_loader, batch_size=args.batch_size, shuffle=True)
+
+
+val_loader = PascalVOC2012Affinity(args.val_set,  args.voc12_img_folder, args.low_cams_fold, args.high_cams_fold,
+                              args.input_dim, args.device,
+
+                              img_transform=transforms.Compose([
+                              transforms.Resize((args.input_dim, args.input_dim)),
+                              transforms.ToTensor(),
+                              normalize,
+                              ]),
+                             label_transform=transforms.Compose([
+                                 transforms.ToTensor(),
+                                 ResizeMultipleChannels((args.input_dim, args.input_dim), mode='bilinear'),
+                             ]))
+
+val_loader = DataLoader(val_loader, batch_size=args.batch_size, shuffle=False)
 
 
 model = ViT_hybrid_model_Affinity(max_epochs=args.epochs, device=args.device)
@@ -65,14 +71,38 @@ model = ViT_hybrid_model_Affinity(max_epochs=args.epochs, device=args.device)
 
 model.load_pretrained(args.pretrained_weights)
 model.session_name = "PascalVOC_classification_Hybrid_Affinity_1"
-model.eval()
+
+if not os.path.exists(model.session_name):
+    os.makedirs(model.session_name)
+
+#
+# # Prepare optimizer and scheduler
+# optimizer = torch.optim.SGD(model.parameters(),
+#                             lr=args.lr,
+#                             momentum=0.9,
+#                             weight_decay=args.weight_decay)
+#
+#
+# for index in range(model.max_epochs):
+#
+#     for g in optimizer.param_groups:
+#         g['lr'] = args.lr * (1-index/model.max_epochs)
+#
+#     print("Training epoch...")
+#     model.train_epoch(train_loader, optimizer)
+#
+#     print("Validating epoch...")
+#     model.val_epoch(val_loader)
+#
+#     model.visualize_graph()
+#
+#     if model.val_history["loss"][-1] < model.min_val:
+#         print("Saving model...")
+#         model.min_val = model.val_history["loss"][-1]
+#
+#         torch.save(model.state_dict(), model.session_name+"/stage_1.pth")
+#
 
 
-for data in train_loader:
 
-    img = data[1]
-
-    model(img)
-
-    print("")
 
